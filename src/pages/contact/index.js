ea867a2a4d4f0c389ba1051d6c065d4e2a125ab1
Jesus Rodriguez <jesus.roga@gmail.com>
2023-05-09 20:00:24 -0600

    first commit

---
+++ src/pages/contact/index.js
@@ -0,0 +1,3 @@
+export default function Contact(){
+    return <h1>Contact Page Modified New</h1>
+}
---
+++ src/pages/projects/index.js
@@ -0,0 +1,3 @@
+export default function Prjects(){
+    return <h1>Projects Page Modified New</h1>
+}
